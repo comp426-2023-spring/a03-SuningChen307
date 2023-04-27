79debcfc7684492a62d0694316e142c754df3d41
SuningChen <tuning@live.unc.edu>
2023-04-27 01:32:05 -0400

    help message

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -3,3 +3,15 @@ import { rps } from 'node-rpsls'
 import minimist from 'minimist'
 
 const args = minimist(process.argv.slice(2));
+
+function help_message() {
+    console.log('Usage: node-rps [SHOT]\n
+    Play Rock Paper Scissors (RPS)\n\n',
+    '  -h, --help      display this help message and exit\n',
+    '  -r, --rules     display the rules and exit\n\nExamples:\n',
+    Examples:
+    '  node-rps        Return JSON with single player RPS result.\n',
+    '                  e.g. {"player":"rock"}\n',
+    '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
+    '                  e.g {"player":"rock","opponent":"scissors","result":"win"}')
+}
