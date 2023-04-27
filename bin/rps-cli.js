2eb11d1b2d8958581f28bf1ca32b23312237edad
Suning Chen <69748174+SuningChen307@users.noreply.github.com>
2023-04-27 16:03:26 -0400

    Update rps-cli.js
    fix10

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -8,7 +8,6 @@ function help_message() {
     console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n',
     '  -h, --help      display this help message and exit\n',
     '  -r, --rules     display the rules and exit\n\nExamples:\n',
-    Examples:
     '  node-rps        Return JSON with single player RPS result.\n',
     '                  e.g. {"player":"rock"}\n',
     '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
